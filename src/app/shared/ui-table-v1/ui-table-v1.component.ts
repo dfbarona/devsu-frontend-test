a724fde098c7f5bb03409df419fa4919f28f5fbe
David Barona <davidfbarona@gmail.com>
2023-12-04 10:56:44 -0500

    feature/layout-page and ui-table

---
+++ src/app/shared/ui-table-v1/ui-table-v1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ui-table-v1',
+  standalone: true,
+  imports: [],
+  templateUrl: './ui-table-v1.component.html',
+  styleUrl: './ui-table-v1.component.css'
+})
+export class UiTableV1Component {
+
+}
